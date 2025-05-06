0df907480934ad33e20c7d2f5603a610979302cd
kanunigames <support@kanunigames.com>
2025-05-06 10:46:08 +0300

    Initial Commit

---
+++ mindscapesvr/mindscapesvrApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindscapesvrApp.swift
+//  mindscapesvr
+//
+//  Created by Sezer Maden on 6.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct mindscapesvrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mindscapesvrTests/mindscapesvrTests.swift
@@ -0,0 +1,17 @@
+//
+//  mindscapesvrTests.swift
+//  mindscapesvrTests
+//
+//  Created by Sezer Maden on 6.05.2025.
+//
+
+import Testing
+@testable import mindscapesvr
+
+struct mindscapesvrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
